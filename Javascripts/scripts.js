7d4b5ef9b670a7657131683781bbbff0612cb3e6
Steven Creswick <screswick@gmail.com>
2015-08-05 15:17:16 -0400

    Adds clearboard function

--- Javascripts/scripts.js
+++ Javascripts/scripts.js
@@ -7,6 +7,8 @@ var gameboard = [
   [null, null, null]    //gameboard[2]
 ]
 
+console.log(gameboard);
+
 function TicTacToe(gameboard){
   this.active = true;
   this.turnsleft = 9;
@@ -25,8 +27,23 @@ TicTacToe.prototype.placePiece = function placePiece(row, col){
     gameboard[row][col] = 'O';
     this.turn = 'X';
   }
-
 }
+
+TicTacToe.prototype.clearBoard = function clearBoard(){
+  gameboard = [
+    [null, null, null],   //gameboard[0]
+    [null, null, null],   //gameboard[1]
+    [null, null, null]    //gameboard[2]
+  ]
+};
 
 var game = new TicTacToe(gameboard);
 game.placePiece(0,0);
+game.placePiece(2,2);
+console.log(gameboard[0]);
+console.log(gameboard[1]);
+console.log(gameboard[2]);
+game.clearBoard();
+console.log(gameboard[0]);
+console.log(gameboard[1]);
+console.log(gameboard[2]);
